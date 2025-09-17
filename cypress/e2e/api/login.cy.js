4823fb379b2c5aa50e9bb93105a51aaf832c5ad9
KarolGuerini <karoltech20@gmail.com>
2025-09-17 16:23:33 -0300

    Criacao de teste de login invalido na api

--- cypress/e2e/api/login.cy.js
+++ cypress/e2e/api/login.cy.js
@@ -2,13 +2,33 @@ describe('API - Login', () => {
   it('Deve logar com usuário válido', () => {
     const apiUrl = Cypress.env('apiUrl')
 
-    cy.request('POST', `${apiUrl}/login`, {
-      email: "fulano@qa.com",
-      password: "teste"
+   cy.request({
+      method: 'POST',
+      url: `${apiUrl}/login`,
+      body: {
+        email: "fulano@qa.com",
+        password: "teste"
+      }
     }).then((res) => {
       expect(res.status).to.eq(200)
       expect(res.body.message).to.eq('Login realizado com sucesso')
       expect(res.body.authorization).to.exist
+    })
+  })
+it('Não deve logar com usuário inválido', () => {
+    const apiUrl = Cypress.env('apiUrl')
+
+    cy.request({
+      method: 'POST',
+      url: `${apiUrl}/login`,
+      body: {
+        email: "usuario_invalido@teste.com",
+        password: "senhaErrada"
+      },
+      failOnStatusCode: false
+    }).then((res) => {
+      expect(res.status).to.eq(401)
+      expect(res.body.message).to.eq('Email e/ou senha inválidos')
     })
   })
 })
